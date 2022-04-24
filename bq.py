220e8aafd723972611889097654b595607bc4aa9
Mike Thisyamondol <thisyamondolm@gmail.com>
2022-04-24 17:44:05 +0000

    bq

--- bq.py
+++ bq.py
@@ -7,58 +7,9 @@ def bq(query):
 
     client = bigquery.Client()
 
+    query_job = client.query(query)
+    df = query_job.to_dataframe()
+    json = df.to_json(orient='records')
 
-
-    dataframe = (
-        client.query(query)
-        .result()
-        .to_dataframe(
-            # Optionally, explicitly request to use the BigQuery Storage API. As of
-            # google-cloud-bigquery version 1.26.0 and above, the BigQuery Storage
-            # API is used by default.
-            create_bqstorage_client=True,
-        )
-    )
-
-    return dataframe
-
-
-query = '''
-with double_entry_book as (
-    -- debits
-    select to_address as address, value as value
-    from `bigquery-public-data.crypto_ethereum.traces`
-    where to_address is not null
-    and status = 1
-    and (call_type not in ('delegatecall', 'callcode', 'staticcall') or call_type is null)
-    union all
-    -- credits
-    select from_address as address, -value as value
-    from `bigquery-public-data.crypto_ethereum.traces`
-    where from_address is not null
-    and status = 1
-    and (call_type not in ('delegatecall', 'callcode', 'staticcall') or call_type is null)
-    union all
-    -- transaction fees debits
-    select 
-        miner as address, 
-        sum(cast(receipt_gas_used as numeric) * cast((receipt_effective_gas_price - coalesce(base_fee_per_gas, 0)) as numeric)) as value
-    from `bigquery-public-data.crypto_ethereum.transactions` as transactions
-    join `bigquery-public-data.crypto_ethereum.blocks` as blocks on blocks.number = transactions.block_number
-    group by blocks.number, blocks.miner
-    union all
-    -- transaction fees credits
-    select 
-        from_address as address, 
-        -(cast(receipt_gas_used as numeric) * cast(receipt_effective_gas_price as numeric)) as value
-    from `bigquery-public-data.crypto_ethereum.transactions`
-)
-select address, sum(value) as balance
-from double_entry_book
-group by address
-order by balance desc
-limit 100;
-'''
-
-print(bq(query))
+    return json
 
