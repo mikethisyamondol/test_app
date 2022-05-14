8d1faff92cbdefa3944eecbcbb7311f7617f64f0
Mike Thisyamondol <thisyamondolm@gmail.com>
2022-05-14 11:57:11 -0400

    post test

--- main.py
+++ main.py
@@ -9,13 +9,10 @@ app = FastAPI()
 async def root():
     return {"message": "Welcome"}
 
-# @app.get("/answer/{question}/{context}")
-# async def answer(question: str, context: str):
-#     return qna(question, context)
+@app.post("/answer/{question}/{context}")
+async def answer(question: str, context: str):
+    return qna(question, context)
 
-@app.post("/answer/")
-async def answer(question: Question, context: Context):
-    return qna(question=question.input, context=context.input)
 
 if __name__ == '__main__':
     uvicorn.run(app, port=8080, host='0.0.0.0')
