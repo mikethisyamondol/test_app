5f45eef18259170d61cb4ff957aca7cade1687cb
Mike Thisyamondol <thisyamondolm@gmail.com>
2022-05-15 13:17:40 -0400

    indent

--- main.py
+++ main.py
@@ -12,14 +12,13 @@ async def root():
 
 @app.post("/answer/{question}/{context}")
 async def answer(question: str, context: str):
-    # return qna(question, context)
     model_name = "deepset/roberta-base-squad2"
-	nlp = pipeline('question-answering', model=model_name, tokenizer=model_name)
-	input = {'question': question,
+    nlp = pipeline('question-answering', model=model_name, tokenizer=model_name)
+    input = {'question': question,
 			'context': context}
-	result = nlp(input)
-	return {'Answer': result}
-
+    result = nlp(input)
+    return {'Answer': result}
+    # return qna(question, context)
 
 if __name__ == '__main__':
     uvicorn.run(app, port=8080, host='0.0.0.0')
