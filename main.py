e1ad83d814aa282ecaac2f1fab41c2673ff56de9
Mike Thisyamondol <thisyamondolm@gmail.com>
2022-05-27 11:44:39 -0400

    api

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 # from question_answering import qna
 from fastapi import FastAPI
 import uvicorn
-from transformers import pipeline
+import requests
+import json
+# from transformers import pipeline
 
 
 app = FastAPI()
@@ -12,17 +14,12 @@ async def root():
 
 @app.get("/answer/{question}/{context}")
 async def answer(question: str, context: str):
-    print('hello0')
-    model_name = "deepset/roberta-base-squad2"
-    print('hello1')
-    nlp = pipeline('question-answering', model=model_name, tokenizer=model_name)
-    print('hello2')
-    qa_input = {'question': question, 'context': context}
-    print('hello3')
-    result = nlp(qa_input)
-    print('hello4')
-    return {'Answer': result}
-    # return qna(question, context)
+    API_URL = "https://api-inference.huggingface.co/models/deepset/roberta-base-squad2"
+    headers = {"Authorization": f"Bearer {${{ secrets.HF_KEY }}}"}
+    payload = {'question': question, 'context': context}
+    response = requests.post(API_URL, headers=headers, json=payload)
+	return response.json()
+   
 
 if __name__ == '__main__':
     uvicorn.run(app, port=8080, host='0.0.0.0')
